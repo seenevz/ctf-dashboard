4ddc91cf825cd36885a61295ceb60d8c1406d7ec
GCdM <59828466+GCdM@users.noreply.github.com>
2020-02-28 16:47:27 +0000

    Changed method names for ActiveRecord associations to be more idiomatic

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,11 @@
 class Board < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, class_name: 'User', foreign_key: :user_id
   has_many :flags
   has_many :game_states
-  has_many :users, through: :game_states
+  has_many :single_players, through: :game_states, class_name: 'User', source: :team, source_type: 'User'
+  has_many :team_players, through: :game_states, class_name: 'Team', source: :team, source_type: 'Team'
+
+  def players
+    self.single_players + self.team_players
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
   has_many :memberships
-  has_many :users, through: :memberships
+  has_many :members, through: :memberships, class_name: 'User', source: :user
   has_many :game_states, as: :team
 end
