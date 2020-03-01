fc9c5b492affe9395904899c01e88f939c91e321
Sergio Neves <sernevess@gmail.com>
2020-03-01 20:40:57 +0000

    Setup session login actions in auth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
 
+
   def landing
     render :landing, layout: false
+  end
+
+  private
+
+  def current_user
+    @current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
   end
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,21 @@
 class AuthController < ApplicationController
+  def login
+  end
+
+  def create_session
+    if user = User.authenticate(login_params)
+      session[:current_user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = "Wrong username/password"
+      render :login
+    end
+  end
+
+  def destroy_session
+  end
+  private
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
 end
