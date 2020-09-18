295e368730b96ba0ba9810495bc189db3573a49b
Sergio Neves <sernevess@gmail.com>
2020-09-18 15:10:06 +0100

    converted board table partial into react component;

---
+++ app/javascript/packs/show_board/boardTable.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const BoardTable = () => {
+  return (
+    <table className="user-boards-table">
+      <thead>
+        <th>Title</th>
+        <th>Published?</th>
+      </thead>
+    </table>
+  );
+};
+
---
+++ app/javascript/packs/show_board/showBoard.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const ShowBoard = () => {
+  return <></>
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <ShowBoard />,
+    document.querySelector("section.display-container")
+  );
+});
