58a44b53c74482cf21ab585dc4bbbbcfb8ac4acd
Sergio Neves <sernevess@gmail.com>
2020-09-18 10:29:28 +0100

    Added placeholder to flag input; Changed flag numbering display;

--- app/javascript/packs/board_form/flagInput.jsx
+++ app/javascript/packs/board_form/flagInput.jsx
@@ -1,24 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const FlagInput = ({ index, flag, changeFlag, removeFlag }) => {
-    const flagId = `flag-${index}`
-    
-    return (
-        <>
-            <label htmlFor={flagId}>{" |> ( "}{index + 1}{" ) |> "}</label>
-            <input 
-                id={flagId}
-                type="text"
-                value={flag}
-                onChange={(e) => changeFlag(e.target.value, index)}
-            />
-            <button onClick={(e) => {
-                e.preventDefault()
-                removeFlag(index)
-            }}>X</button>
-            <br/>
-        </>
-    )
-}
+  const flagId = `flag-${index}`;
 
-export default FlagInput
+  const numeration = index => {
+    const pos = ++index;
+    let lastDigit = pos;
+
+    if (pos > 20) {
+      lastDigit = parseInt(pos.toString().slice(-1));
+    }
+
+    switch (lastDigit) {
+      case 1:
+        return `${pos}st`;
+      case 2:
+        return `${pos}nd`;
+      case 3:
+        return `${pos}rd`;
+      default:
+        return `${pos}th`;
+    }
+  };
+
+  return (
+    <>
+      <label htmlFor={flagId}>
+        {" |> ( "}
+        {numeration(index)}
+        {" ) |> "}
+      </label>
+      <input
+        id={flagId}
+        type="text"
+        value={flag}
+        placeholder="Flag value goes here"
+        onChange={e => changeFlag(e.target.value, index)}
+      />
+      <button
+        onClick={e => {
+          e.preventDefault();
+          removeFlag(index);
+        }}
+      >
+        X
+      </button>
+      <br />
+    </>
+  );
+};
+
+export default FlagInput;
